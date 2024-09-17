8dd346cbe8977d9b630faa37c030eb2136ef45ee
Sonny <60908891+iffyspeak-chroma@users.noreply.github.com>
2024-09-17 08:27:08 -0500

    Add a way to identify player by UUID

---
+++ src/main/java/xyz/iffyspeak/servernicknames/Utilities.java
@@ -0,0 +1,20 @@
+package xyz.iffyspeak.servernicknames;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+
+import java.util.UUID;
+
+public class Utilities {
+    public static class Players {
+        public static String getUsername(UUID playerUUID, MinecraftServer server)
+        {
+            PlayerList playerlist = server.getPlayerList();
+
+            ServerPlayer player = playerlist.getPlayer(playerUUID);
+
+            return player != null ? player.getName().getString() : "NOTHINGRETURNED";
+        }
+    }
+}
